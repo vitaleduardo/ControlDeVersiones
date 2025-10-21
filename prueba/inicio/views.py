3052b276390f19e433ece2782181c5b5efcfb028
Eduardo Vital <eduardovital@192.168.1.106>
2025-10-20 20:42:59 -0600

    Agregando proyecto prueba

---
+++ prueba/inicio/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def contacto(request):
+    return render(request, "inicio/contacto.html")
+    
+def registrar(request):
+    return render(request, "inicio/formulario.html")
+
+def principal(request):
+    return render(request, "inicio/principal.html")
